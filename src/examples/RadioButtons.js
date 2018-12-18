5fb7edc6b94b399d098fcb2d406bd37bbc58a383
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2018-12-18 20:14:01 +0800

    Update bordered-radio-button mixin

--- src/examples/RadioButtons.js
+++ src/examples/RadioButtons.js
@@ -14,6 +14,33 @@ const RadioButtons = () => {
           <input type='radio' name='one' value='b' />
           <span>Banana</span>
         </label>
+        <label className={CSS.label}>
+          <input type='radio' name='one' value='c' disabled />
+          <span>Disabled</span>
+        </label>
+      </ExampleBlock>
+
+      <ExampleBlock
+        title='Alternate settings'
+        description={
+          <>
+            Increased <code>size</code>, <code>border-size</code> and{' '}
+            <code>inset-size</code>
+          </>
+        }
+      >
+        <label className={CSS.bigLabel}>
+          <input type='radio' name='one' value='a' />
+          <span>Apple</span>
+        </label>
+        <label className={CSS.bigLabel}>
+          <input type='radio' name='one' value='b' />
+          <span>Banana</span>
+        </label>
+        <label className={CSS.bigLabel}>
+          <input type='radio' name='one' value='c' disabled />
+          <span>Disabled</span>
+        </label>
       </ExampleBlock>
     </div>
   )
